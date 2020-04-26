034a8f085c227efdf2fa2024a0dc2996f972be42
Bruce Tran <bruce.tran@gmail.com>
2020-04-25 22:35:38 -0400

    Update Login Page.

--- BethanysPieShopXamarin/BethanysPieShopXamarin/LoginView.xaml.cs
+++ BethanysPieShopXamarin/BethanysPieShopXamarin/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace BethanysPieShopXamarin
@@ -9,6 +10,12 @@ namespace BethanysPieShopXamarin
 		public LoginView()
 		{
 			InitializeComponent();
+		}
+
+		private async void LoginButton_OnClicked(object sender, EventArgs e)
+		{
+			Navigation.InsertPageBefore(new MainView(), this);
+			await Navigation.PopAsync();
 		}
 	}
 }
